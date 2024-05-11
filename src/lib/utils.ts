0fb729dac902bad48d01390bfb24ecb01ab15006
Riday ♡ <ridayshah2001@gmail.com>
2024-05-11 23:52:51 +0530

    ✨ Feat: utility function to generate random ids

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+// A random ID generator.
+export function generateId() {
+	return Math.random().toString(32).slice(2, 6) + Date.now().toString(32).slice(4, 8);
+}
