5ca56e659c15ca25b17f390c74abead93786264e
Riday ♡ <ridayshah2001@gmail.com>
2024-05-12 10:07:40 +0530

    💬 Feat: change landing page heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import { getPrisma } from '@/lib/db';
 
 export default async function Home() {
 	const prisma = getPrisma();
-	const posts = await prisma.post.findMany({ orderBy: { createdAt: 'desc' }, take: 10 });
+	const posts = await prisma.post.findMany({ orderBy: { createdAt: 'desc' }, take: 20 });
 
 	return (
 		<main>
-			<h1 className='text-center'>Mini Gallery</h1>
+			<h1 className='text-center'>Recent Posts</h1>
 			<Posts posts={posts} className='grid-cols-3 md:grid-cols-4 xl:grid-cols-5' />
 		</main>
 	);
