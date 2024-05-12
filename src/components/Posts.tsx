105bcaeecbcf5160d1d84e98d0f264a52a7499f1
Riday ♡ <ridayshah2001@gmail.com>
2024-05-12 11:24:18 +0530

    🎨 Refactor: resize images properly

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -3,15 +3,33 @@
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import type { Post } from '@prisma/client';
 
 export function Posts({ posts, className = '' }: PostsProps) {
-	const [clientWidth, setClientWidth] = useState(100);
+	const [clientWidth, setClientWidth] = useState(DEFAULT_WIDTH);
+	const sectionRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (!sectionRef?.current) return null;
+
+		const setWidth = () => setClientWidth(sectionRef.current.querySelector('img')?.clientWidth ?? DEFAULT_WIDTH);
+
+		// Run on first render.
+		setWidth();
+
+		// Run whenever the window is resized.
+		if (typeof window !== 'undefined') window.addEventListener('resize', setWidth);
+
+		return () => {
+			// Cleanup.
+			if (typeof window !== 'undefined') window.removeEventListener('resize', setWidth);
+		};
+	}, [sectionRef]);
 
 	return (
-		<section className={cn('mt-6 grid grid-cols-3 gap-4 pb-6', className)}>
+		<section className={cn('mt-6 grid grid-cols-3 gap-4 pb-6', className)} ref={sectionRef}>
 			{posts.length < 0 && <p className='text-center'>No posts yet.</p>}
 			{posts.map(post => (
 				<Link key={post.id} className='relative' style={{ height: clientWidth }} href={`/posts/${post.id}`}>
@@ -24,7 +42,6 @@ export function Posts({ posts, className = '' }: PostsProps) {
 						blurDataURL={post.blurUrl}
 						key={post.id}
 						loading='lazy'
-						onLoad={e => setClientWidth(e.currentTarget.clientWidth)}
 					/>
 				</Link>
 			))}
@@ -36,3 +53,5 @@ type PostsProps = {
 	posts: Array<Post>;
 	className?: string;
 };
+
+const DEFAULT_WIDTH = 100;
