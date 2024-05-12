8d358883dc0e3508d98125aa4b0c3db83267fa34
Riday ♡ <ridayshah2001@gmail.com>
2024-05-12 08:19:27 +0530

    ⚡️ Feat: add caching

--- src/app/posts/raw/[id]/route.ts
+++ src/app/posts/raw/[id]/route.ts
@@ -2,11 +2,15 @@ import { getStore } from '@netlify/blobs';
 
 export async function GET(_req: Request, { params: { id } }: GetRawPostProps) {
 	const store = getStore('posts');
-	const file = await store.get(id, { type: 'blob' });
+	const { data, metadata } = await store.getWithMetadata(id, { type: 'blob' });
+	if (!data) return new Response('Not found', { status: 404 });
 
-	if (!file) return new Response('Not found', { status: 404 });
-
-	return new Response(file);
+	return new Response(data, {
+		headers: {
+			'Netlify-CDN-Cache-Control': 'public, s-maxage=31536000, must-revalidate',
+			'Netlify-Cache-Tag': [id, metadata.authorId ?? ''].join(',')
+		}
+	});
 }
 
 type GetRawPostProps = {
